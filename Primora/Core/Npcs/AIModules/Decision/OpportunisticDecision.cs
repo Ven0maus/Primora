f2b78e2c2bc6bec01698acd508a4b4291362b866
venomaus <stefvandenbruel@outlook.com>
2025-09-27 20:28:15 +0200

    Some cleanup

--- Primora/Core/Npcs/AIModules/Decision/OpportunisticDecision.cs
+++ Primora/Core/Npcs/AIModules/Decision/OpportunisticDecision.cs
@@ -2,6 +2,7 @@
 using Primora.Core.Npcs.Objects;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Primora.Core.Npcs.AIModules.Decision
 {
@@ -12,55 +13,53 @@ namespace Primora.Core.Npcs.AIModules.Decision
             Actor easiestTarget = null;
             int lowestHP = int.MaxValue;
 
-            if (self.AIController.CurrentTarget == null)
+            // Clear target if no longer in detection
+            if (self.AIController.CurrentTarget != null && detectedTargets.Contains(self.AIController.CurrentTarget))
             {
-                foreach (var target in detectedTargets)
-                {
-                    if (target.Stats.Health <= 0 || target.Faction == self.Faction)
-                        continue;
-
-                    // If health is too high we don't want to consider this target as viable
-                    if (target.Stats.Health > (int)Math.Round(target.Stats.MaxHealth * 0.5f))
-                    {
-                        // TODO: Check if the target is hostile and can sense us then chance to flee else skip
-                        if (target.IsHostileTowards(self) && (target.AIController == null || target.AIController.IsAwareOf(self)))
-                        {
-                            if (Random.Shared.NextDouble() < 0.3)
-                            {
-                                self.AIController.CurrentTarget = target;
-                                self.AIController.AIState = AIState.Flee;
-                            }
-                            continue;
-                        }
-                        else
-                        {
-                            // Sometimes take a risk
-                            if (Random.Shared.NextDouble() >= 0.1)
-                            {
-                                continue;
-                            }
-                        }
-                    }
-
-                    // Start sorting
-                    if (target.Stats.Health < lowestHP)
-                    {
-                        lowestHP = target.Stats.Health;
-                        easiestTarget = target;
-                    }
-                }
-
-                if (easiestTarget != null)
-                {
-                    self.AIController.CurrentTarget = easiestTarget;
-                    self.AIController.AIState = self.DistanceTo(easiestTarget.Position) <= self.Stats.AttackRange
-                            ? AIState.Combat : AIState.Chase;
-                }
-                else
-                {
+                if (self.AIController.CurrentTarget.Stats.Health <= 0)
                     self.AIController.CurrentTarget = null;
-                    self.AIController.AIState = AIState.Wander;
+                else
+                    return;
+            }
+            else if (self.AIController.CurrentTarget != null)
+                self.AIController.CurrentTarget = null;
+
+            foreach (var target in detectedTargets)
+            {
+                if (target.Stats.Health <= 0 || target.Faction == self.Faction)
+                    continue;
+
+                // If health is too high we don't want to consider this target as viable
+                if (target.Stats.Health > (int)Math.Round(target.Stats.MaxHealth * 0.5f))
+                {
+                    // Check if the target is hostile and can sense us then chance to flee else skip
+                    if (target.IsHostileTowards(self) && (target.AIController == null || target.AIController.IsAwareOf(self)))
+                    {
+                        self.AIController.CurrentTarget = target;
+                        self.AIController.AIState = AIState.Flee;
+                        return;
+                    }
+                    continue;
                 }
+
+                // Start sorting
+                if (target.Stats.Health < lowestHP)
+                {
+                    lowestHP = target.Stats.Health;
+                    easiestTarget = target;
+                }
+            }
+
+            if (easiestTarget != null)
+            {
+                self.AIController.CurrentTarget = easiestTarget;
+                self.AIController.AIState = self.DistanceTo(easiestTarget.Position) <= self.Stats.AttackRange
+                        ? AIState.Combat : AIState.Chase;
+            }
+            else
+            {
+                self.AIController.CurrentTarget = null;
+                self.AIController.AIState = AIState.Wander;
             }
 
             if (self.AIController.CurrentTarget != null && self.AIController.CurrentTarget.Stats.Health <= 0)
